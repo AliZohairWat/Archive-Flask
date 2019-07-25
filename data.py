5ea76d358f749f7b5525982c97387c583e40d052
Ali Zohair <46579403+AliZohairWat@users.noreply.github.com>
2019-07-25 19:43:39 -0400

    Added multiple new feature (about/articles/user registration/database)
    Updated app.py/home.html/layout/html with bootstrap templates
    Created about/article routes
    Created database code and added sample data file - data.py
    Added partially developed registration mechanism

---
+++ data.py
@@ -0,0 +1,19 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, soluta! Nesciunt vitae quo porro reprehenderit fugit. Beatae culpa consequuntur commodi enim consequatur nam. Mollitia maxime accusamus et. Mollitia, iure illum.', 
+            'author': 'Ali Zohair',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, soluta! Nesciunt vitae quo porro reprehenderit fugit. Beatae culpa consequuntur commodi enim consequatur nam. Mollitia maxime accusamus et. Mollitia, iure illum.', 
+            'author': 'Alina Zehra',
+            'create_date': '05-13-2019'
+        }
+
+    ]
+    return articles
